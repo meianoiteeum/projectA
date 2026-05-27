aa24260e0557033ceb9af8a04f6c35bb0f883204
Leo Saraiva <leosaraiva.cont@gmail.com>
2026-05-26 21:42:04 -0300

    Add: Blocking level 00

--- Assets/Script/Gameplay/Characters/PlayerSpawner.cs
+++ Assets/Script/Gameplay/Characters/PlayerSpawner.cs
@@ -37,9 +37,10 @@ namespace Script.Gameplay.Characters
             pc.Init(startNode.id, mapBuilder, mapData, onReachEnd);
             playerObject.name = "Player";
 
-            //var cam = Camera.main;
-            //if (cam != null && cam.GetComponent<CameraFollow>() == null)
-               // cam.gameObject.AddComponent<CameraFollow>();
+// Codigo da camera
+            var cam = Camera.main;
+            if (cam != null && cam.GetComponent<CameraFollow>() == null)
+               cam.gameObject.AddComponent<CameraFollow>();
 
             var playerTransform = playerObject.transform;
             
