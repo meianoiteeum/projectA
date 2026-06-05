d343fa38815df46b2f953e891c07b73b01772607
vitória-paiz <vitoriapaiz14@gmail.com>
2026-06-05 15:28:43 -0300

    Ajuste do menu de opções preencher toda a tela nos leveis + ajuste do pause durante as animações de transições - desabilitado (espera animação concluir para habilitar)

--- Assets/Script/Util/ScreenTransition.cs
+++ Assets/Script/Util/ScreenTransition.cs
@@ -5,15 +5,36 @@ public class ScreenTransition : MonoBehaviour
 {
     [SerializeField] private Animator anim;
     string sceneNameToLoad;
+    
+    // Propriedade para checar se a transição está acontecendo
+    public bool IsTransitioning { get; private set; }
+
+    private void Start()
+    {
+        IsTransitioning = true;
+        anim.Update(0); 
+        float fadeInDuration = anim.GetCurrentAnimatorStateInfo(0).length;
+        Invoke(nameof(EndTransition), fadeInDuration);
+    }
 
     public void LoadScene(string sceneName)
     {
+        if (IsTransitioning) return; 
+
+        IsTransitioning = true; 
+        
         anim.Play("FadeOut");
         sceneNameToLoad = sceneName;
         Invoke(nameof(LoadSceneAfter), anim.GetCurrentAnimatorStateInfo(0).length);
     }
+    
     void LoadSceneAfter()
     {
         SceneManager.LoadScene(sceneNameToLoad);
+    }
+    
+    void EndTransition()
+    {
+        IsTransitioning = false;
     }
 }
